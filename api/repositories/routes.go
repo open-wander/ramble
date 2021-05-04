997d306b895148d1036cacfe6813757b6603bcca
Lance Haig <lnhaig@gmail.com>
2021-05-04 10:03:02 +0200

    Add Redirect for the Nomad Clients and some data for making pagination easier

--- api/repositories/routes.go
+++ api/repositories/routes.go
@@ -7,7 +7,7 @@ import (
 func Routes(route fiber.Router) {
 	route.Get("/_catalog", GetAllRepositories)
 	route.Get("/:user", GetUserRepositories)
-	route.Get("/:user/:name", GetRepository)
+	route.Get("/:user/:name/*", GetRepository)
 	route.Put("/:user/:name", UpdateRepository)
 	route.Post("/:user", NewRepository)
 	route.Delete("/:user/:name", DeleteRepository)
