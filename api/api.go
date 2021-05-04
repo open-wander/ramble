7a626f2106443719f84b9df822742ac6871b6859
Lance Haig <lnhaig@gmail.com>
2021-05-04 10:01:29 +0200

    Remove the v1 from the link to tidy up the calls

--- api/api.go
+++ api/api.go
@@ -8,13 +8,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func version(c *fiber.Ctx) error {
-	return c.SendString("v1")
-}
-
 func Setup(app *fiber.App) {
-	v1 := app.Group("/v1")
-	v1.Get("/", version)
+	v1 := app.Group("/")
 	repositories.Routes(v1)
 	user := app.Group("/user")
 	users.Routes(user)
