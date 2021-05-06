da97f064655813668ce7a541ca0fecf05dbf3270
Lance Haig <lnhaig@gmail.com>
2021-05-06 15:23:55 +0200

    Fix The Redirect for the repo URL

--- models/user-model.go
+++ models/user-model.go
@@ -5,9 +5,9 @@ import "rmbl/pkg/database"
 // User struct
 type User struct {
 	database.DefaultModel
-	Username     string       `json:"username"`
-	Email        string       `json:"email"`
-	Password     string       `json:"password"`
-	Names        string       `json:"names"`
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	role         string
 	Repositories []Repository `json:"repositories"`
 }
