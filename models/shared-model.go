b9ef3b0b44b3799a7fd517dbd1af29dda87c4fe6
Lance Haig <lnhaig@gmail.com>
2021-06-04 16:42:09 +0200

    Fix a Bug in the Signup Code

--- models/shared-model.go
+++ models/shared-model.go
@@ -8,21 +8,12 @@ type OrgData struct {
 	Data         []Organization
 }
 
-// // Repo Data is generated for filtering and pagination of Repositories
-// type RepoData struct {
-// 	TotalData    int64
-// 	FilteredData int64
-// 	Data         []RepositoryViewStruct
-// 	// Data []Organization
-// }
-
 // Repo Data is generated for filtering and pagination of Repositories
 type RepoData struct {
 	Status       string
 	Message      string
 	TotalRecords int64
-	Data         []Repository
-	// Data []Organization
+	Data         []RepositoryViewStruct
 }
 
 // User Data is generated for filtering and pagination of Users
